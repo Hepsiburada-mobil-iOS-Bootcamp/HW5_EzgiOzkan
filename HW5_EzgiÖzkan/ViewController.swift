9fb4628fa7e2777e93a2df4260d45b299a68572b
ezgi <ezgiozkan9898@gmail.com>
2021-10-23 10:03:37 +0300

    Initial Commit

---
+++ HW5_EzgiÖzkan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW5_EzgiÖzkan
+//
+//  Created by ezgi on 23.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
